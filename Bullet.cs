6feafa931683148e1916f870dffed6a0b43aa63d
Egor Babaev <97120614+bizazin@users.noreply.github.com>
2022-04-02 20:47:09 +0300

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -7,15 +7,19 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _bounceForce;
     [SerializeField] private float _bouceRadius;
+    
     private Vector3 _moveDirection;
+    
     private void Start()
     {
         _moveDirection = Vector3.forward;
     }
+    
     private void Update()
     {
         transform.Translate(_moveDirection * _speed * Time.deltaTime);
     }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Block block))
@@ -23,11 +27,11 @@ public class Bullet : MonoBehaviour
             block.Break();
             Destroy(gameObject);
         }
+        
         if (other.TryGetComponent(out Obstacle obstacle))
-        {
             Bounce();
-        }
     }
+    
     private void Bounce()
     {
         _moveDirection = Vector3.back + Vector3.up;
