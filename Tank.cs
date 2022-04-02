a36b05a7e9276f708ce0f46c95db45437451aaad
Egor Babaev <97120614+bizazin@users.noreply.github.com>
2022-04-02 20:49:23 +0300

    Update Tank.cs

--- Tank.cs
+++ Tank.cs
@@ -10,20 +10,19 @@ public class Tank : MonoBehaviour
     [SerializeField] private float _recoilDistance;
 
     private float _timeAfterShoot;
+    
     private void Update()
     {
         _timeAfterShoot += Time.deltaTime;
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && _timeAfterShoot > _delayBetweenShoots)
         {
-            if (_timeAfterShoot > _delayBetweenShoots)
-            {
                 Shoot();
                 transform.DOMoveZ(transform.position.z - _recoilDistance, _delayBetweenShoots/2).SetLoops(2, LoopType.Yoyo);
                 _timeAfterShoot = 0;
-            }
         }
     }
+    
     private void Shoot()
     {
         Instantiate(_bulletTemplate, _shootPoint.position, Quaternion.identity);
