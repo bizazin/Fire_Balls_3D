1347e8ec295b22e901cd97cd1c580887b4c595bd
Egor Babaev <97120614+bizazin@users.noreply.github.com>
2022-04-02 20:46:38 +0300

    Update Block.cs

--- Block.cs
+++ Block.cs
@@ -8,14 +8,17 @@ public class Block : MonoBehaviour
     [SerializeField] private ParticleSystem _destroyEffect;
     private MeshRenderer _meshRenderer;
     public event UnityAction<Block> BulletHit;
+    
     private void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
     }
+    
     public void SetColor(Color color)
     {
         _meshRenderer.material.color = color;
     }
+    
     public void Break()
     {
         BulletHit?.Invoke(this);
